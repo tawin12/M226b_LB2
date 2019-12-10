732d45a483a1b3a2f02b57ed822c72bb7f29c845
Tawin12 <luca.zaczkowsky@gmail.com>
2019-12-10 16:43:47 +0100

    Commit basic structure

---
+++ angular-tetris/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { StartComponent } from './start/start.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BoardComponent,
+    StartComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
