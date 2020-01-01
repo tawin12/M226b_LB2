d16b516ee83a73d68d8d7c06bb8b49fd8f6f14d8
Tawin12 <luca.zaczkowsky@gmail.com>
2020-01-01 16:37:44 +0100

    Canvas für spielboard erstellt

--- angular-tetris/src/app/board/board.component.ts
+++ angular-tetris/src/app/board/board.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { COLS, BLOCK_SIZE, ROWS } from './constants';
 
 @Component({
   selector: 'app-board',
@@ -9,7 +10,38 @@ export class BoardComponent implements OnInit {
 
   constructor() { }
 
+  @ViewChild('board', { static: true })
+  canvas: ElementRef<HTMLCanvasElement>;
+
+  ctx: CanvasRenderingContext2D;
+  points: number;
+  lines: number;
+  level: number;
+
   ngOnInit() {
+
+    this.initBoard();
+
   }
+
+  getEmptyBoard(): number[][] {
+    return Array.from({ length: ROWS }, () => Array(COLS).fill(0));
+  }
+
+  initBoard() {
+    // Get the 2D context that we draw on.
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+
+    // Calculate size of canvas from constants.
+    this.ctx.canvas.width = COLS * BLOCK_SIZE;
+    this.ctx.canvas.height = ROWS * BLOCK_SIZE;
+  }
+
+  play() {
+
+    console.table(this.getEmptyBoard);
+
+  }
+
 
 }
